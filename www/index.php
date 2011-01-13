c39062eadbb3864975c31db275873bc7f6b47f44
Vincent Voyer <vincent.voyer@gmail.com>
2011-01-13 16:57:24 +0100

    cache bug when invalid url

--- www/index.php
+++ www/index.php
@@ -1,16 +1,17 @@
 <?php
+
 ob_implicit_flush(true);
-require dirname(__FILE__).'/path.php';
+require dirname(__FILE__) . '/path.php';
 require 'Pluf.php';
 
 // Where is your configuration file?
-define('ADMIN_CONFIG_FILE', $path_to_Zblog.'/Zblog/conf/zblog.php');
+define('ADMIN_CONFIG_FILE', $path_to_Zblog . '/Zblog/conf/zblog.php');
 Pluf::start(ADMIN_CONFIG_FILE);
 
 if (count($_POST) === 0 && Pluf::f('debug') === false) {
 	// production environment
 	$cache = Pluf_Cache::factory();
-	if (null === ($foo=$cache->get(Pluf_HTTP_URL::getAction()))) {
+	if (null === ($foo = $cache->get(Pluf_HTTP_URL::getAction()))) {
 		ob_start();
 		// As we are using a dispatcher, we need to load the corresponding
 		// view controllers. The controllers are just a mapping between the query
@@ -23,16 +24,20 @@ if (count($_POST) === 0 && Pluf::f('debug') === false) {
 		$foo = ob_get_contents();
 		ob_end_clean();
 
-		// how much time to keep the cache ?
-		$cache_timeout = $dispatch[0]->view[0]['cache_timeout'];
-		if($cache_timeout === null) {
-			$cache_timeout = Pluf::f('cache_timeout'); // default to configuration option
-		}
+		if ($dispatch[1]->status_code === 200) {
+			// store in cache only if the status code is 200
 
-		// store in cache
-		$cache->set(Pluf_HTTP_URL::getAction(), $foo, $cache_timeout);
+			// how much time to keep the cache ?
+			$cache_timeout = $dispatch[0]->view[0]['cache_timeout'];
+			if ($cache_timeout === null) {
+				$cache_timeout = Pluf::f('cache_timeout'); // default to configuration option
+			}
+
+			// store in cache
+			$cache->set(Pluf_HTTP_URL::getAction(), $foo, $cache_timeout);
+		}
 	}
-	
+
 	echo $foo;
 } else {
 	// when in "debug" mode, most of the time its in home dev
