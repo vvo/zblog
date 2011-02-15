0203c730c4d30996274254760ad113140ea88285
Vincent Voyer <vincent@zeroload.local.local>
2011-02-15 12:05:40 +0100

    article html minification, correction bug lien disqus

--- src/Zblog/Template/DisqusComments.php
+++ src/Zblog/Template/DisqusComments.php
@@ -5,6 +5,6 @@ Pluf::loadFunction('Pluf_HTTP_URL_urlForView');
 class Zblog_Template_DisqusComments extends Pluf_Template_Tag {
 
 	function start($post, $params=array(), $get_params=array()) {
-		echo '<a data-disqus-identifier="'.$post->getDisqusId().'" class="floatRight commentsCount font11" href="'.Pluf_HTTP_URL_urlForView('blog_post', rawurlencode(strtolower($post->title))).'#disqus_thread"></a>';
+		echo '<a data-disqus-identifier="'.$post->getDisqusId().'" class="floatRight commentsCount font11" href="'.$post->getFriendlyUrl().'#disqus_thread"></a>';
 	}
 }
